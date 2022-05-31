15cd893df5bb73078d20daed53a1a069ff8c5c06
Avinash <avinash1234sharma1@gmail.com>
2022-05-31 19:07:39 +0530

    calculator is done

---
+++ Calculator/index.js
@@ -0,0 +1,31 @@
+// console.log(process.argv)
+// add, sub, mult, divide, sin, cos, tan, random
+
+const type = process.argv[2]
+if(type === 'add'){
+    console.log(+process.argv[3]+ +process.argv[4])
+}
+else if( type === 'sub'){
+    console.log(+process.argv[3] - +process.argv[4]);
+}
+else if(type === 'mult'){
+    console.log(+process.argv[3] * +process.argv[4]);
+}
+
+else if(type === 'divide'){
+    console.log(+process.argv[3] / +process.argv[4]);
+}
+else if(type === 'sin'){
+     console.log(Math.sin(+process.argv[3]*0.0175));
+}
+else if(type === 'cos'){
+    console.log(Math.cos(+process.argv[3]*0.0175))
+}
+else if(type === 'tan'){
+    console.log(Math.tan(+process.argv[3]*0.0175))
+}
+else if(type === 'random'){
+     console.log(Math.random()*10)
+}
+
+
