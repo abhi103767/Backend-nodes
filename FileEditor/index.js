9c07258493c68b209bf5832adc54281f803a7ed5
Avinash <avinash1234sharma1@gmail.com>
2022-05-31 22:01:19 +0530

    everything is done except onde

---
+++ FileEditor/index.js
@@ -0,0 +1,49 @@
+// similarly append CONTENT test.txt will append the 'content' at the end of the file
+// delete test.txt will delete the file
+// create test.txt will create a new file
+// rename test.txt new.txt will rename the file
+
+
+const fs = require('fs');
+
+const type = process.argv[2];
+
+
+
+if(type === 'read'){
+     fs.readFile( process.argv[3],'utf8',(error,data) => {
+         if(error) console.log(error);
+         console.log(data);
+     })
+
+}
+
+if(type === 'append'){
+
+    fs.appendFile(process.argv[4], process.argv[3], (error) => {
+      console.log(error)
+    })
+
+}
+if( type === 'delete'){
+
+    fs.unlink(process.argv[3],(error) => {
+        console.log(error)
+    })
+
+}
+if(type === 'create'){
+    fs.writeFile(process.argv[3],'', (error) => {
+        console.log(error)
+    })
+
+}
+if(type === 'rename'){
+   fs.renameSync(process.argv[3],process.argv[4], (error) => {
+       console.log(error)
+   } );
+
+   fs.Fil
+
+}
+
