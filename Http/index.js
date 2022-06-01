b943c7f8b5977a0427d2abb6890a50742845e7eb
Avinash <avinash1234sharma1@gmail.com>
2022-06-01 11:19:43 +0530

    creating server

--- Http/index.js
+++ Http/index.js
@@ -2,13 +2,10 @@ const http = require('http');
 
 const server = http.createServer((req,res) => {
 
-   console.log(req.url)
-
-
-
+ console.log(req)
    if(req.url === '/') res.end('Welcome to homepage');
-   if(req.url === '/products') res.end(JSON.stringify('[1,2,3,4,4]'))
+   if(req.url === '/products') res.end(JSON.stringify([1,2,3,4,4]))
 
-    res.end("Hello World");
+   else res.end('404 ');
 })
 server.listen(8080)
--- another.js
+++
@@ -1,6 +0,0 @@
-function isEven(){
-return 5
-}
-
-
-
